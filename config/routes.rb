33d19258c8b308845d8018c26915334ebc9ddbd6
Tom Walk <tcwalk@gmail.com>
2017-03-27 17:49:51 -0500

    Add hello, world to production route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-   if Rails.env.development?
+   if Rails.env.production?
       root 'application#hello'
    end
 
